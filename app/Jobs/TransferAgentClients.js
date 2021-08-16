f7884bbcd3a599dac7ebafcff259cbfb7e9a816c
leicaflorian <florian.leica@gmail.com>
2021-08-16 11:19:19 +0200

    Fix bug mongo

--- app/Jobs/TransferAgentClients.js
+++ app/Jobs/TransferAgentClients.js
@@ -3,22 +3,31 @@ const CommissionModel = use('App/Models/Commission')
 
 /** @type {typeof import('../Models/User')} */
 const UserModel = use('App/Models/User')
+const Database = use('Database')
 
 const {castToObjectId} = require("../Helpers/ModelFormatters")
 
 module.exports =
   /** @param {import("../../@types/QueueProvider").QueueJob} job */
   async function (job) {
+    const db = await Database.connect("mongodb")
+
     /**
      * @type {{oldAgent: string, newAgent: string}}
      */
     const data = job.attrs.data
+    const result = await db.collection('users').update(
+      {'referenceAgent': castToObjectId(data.oldAgent)}, //search
+      {'referenceAgent': castToObjectId(data.newAgent)}, // new data
+      {multi: true} // options
+    )
 
-    const result = await UserModel.query()
-      .where('referenceAgent', castToObjectId(data.oldAgent))
-      .update({'referenceAgent': castToObjectId(data.newAgent)})
 
-    job.attrs.result = "toJson" in result ? result.toJSON() : result;
+    /* const result = await UserModel.query()*/
+    /*   .where('referenceAgent', castToObjectId(data.oldAgent))*/
+    /*   .update({'referenceAgent': castToObjectId(data.newAgent)})*/
+
+    job.attrs.result = result;
 
     await job.save()
   }
