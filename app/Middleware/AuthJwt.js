3e6f38d8e657378844752cf961d8c77e07a764ba
leicaflorian <florian.leica@gmail.com>
2022-01-03 15:00:50 +0100

    Updates jwt middleware error show

--- app/Middleware/AuthJwt.js
+++ app/Middleware/AuthJwt.js
@@ -7,27 +7,27 @@ class AuthJwt {
     const scheme = 'jwt'
     let lastError = null
 
-    if (request.hostname() === "localhost" && request.headers()["user-agent"].startsWith("PostmanRuntime") && Env.get("NODE_ENV") === "development") {
+   /* if (request.hostname() === "localhost" && request.headers()["user-agent"].startsWith("PostmanRuntime") && Env.get("NODE_ENV") === "development") {
       return next()
-    }
+    }*/
 
     try {
-      const authenticator = auth.authenticator(scheme)
-      await authenticator.check()
+      const authenticator = auth.authenticator(scheme);
+      await authenticator.check();
 
       /**
        * Swapping the main authentication instance with the one using which user
        * logged in.
        */
-      auth.authenticatorInstance = authenticator
+      auth.authenticatorInstance = authenticator;
 
-      lastError = null
+      lastError = null;
     } catch (error) {
       lastError = error
     }
 
     if (lastError) {
-      throw new TokenExpiredException()
+      throw new TokenExpiredException(lastError.message, lastError.status);
     }
 
     await next()
