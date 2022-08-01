a74320947172b2efe3eeb74f97b709bbccfa51d8
leicaflorian <florian.leica@gmail.com>
2022-08-01 17:39:27 +0200

    Add getDocument method to DocSigner provider

--- providers/DocSigner/index.js
+++ providers/DocSigner/index.js
@@ -5,100 +5,100 @@ const {
   SignrequestQuickCreateApi,
   Signer,
   SignrequestsApi
-} = require('signrequest-client');
-const {SignRequestQuickCreate} = require('signrequest-client');
-const {template: _template, snakeCase} = require("lodash")
+} = require('signrequest-client')
+const { SignRequestQuickCreate } = require('signrequest-client')
+const { template: _template, snakeCase } = require('lodash')
 
-const Env = use("Env")
+const Env = use('Env')
 
-/** @typedef {import("../../@types/User.d").User} IUser */
-/** @typedef {import("../../@types/SignRequest/index.d").Signer} ISigner */
+/** @typedef {import('../../@types/User.d').User} IUser */
+/** @typedef {import('../../@types/SignRequest/index.d').Signer} ISigner */
 
-/** @typedef {import("../../@types/SignRequest/index.d").SignRequestQuickCreate} ISignRequestQuickCreate */
-/** @typedef {import("../../@types/SignRequest/Config.d").TemplateConfig} ITemplateConfig */
+/** @typedef {import('../../@types/SignRequest/index.d').SignRequestQuickCreate} ISignRequestQuickCreate */
+/** @typedef {import('../../@types/SignRequest/Config.d').TemplateConfig} ITemplateConfig */
 
 class DocSigner {
-  constructor(Config) {
+  constructor (Config) {
     /**
-     * @type {import("../../@types/SignRequest/index.d").Config}
+     * @type {import('../../@types/SignRequest/index.d').Config}
      */
-    this._config = Config.get("docSigner")
-
+    this._config = Config.get('docSigner')
+    
     // Configure API key authorization: Token
-    const Token = ApiClient.instance.authentications["Token"];
+    const Token = ApiClient.instance.authentications['Token']
     Token.apiKey = this._config.apiKey
-    Token.apiKeyPrefix = "Token"
-
-    this.DocumentsApi = new DocumentsApi();
-    this.TemplatesApi = new TemplatesApi();
-    this.SignrequestsApi = new SignrequestsApi();
-    this.SignrequestQuickCreateApi = new SignrequestQuickCreateApi();
+    Token.apiKeyPrefix = 'Token'
+    
+    this.DocumentsApi = new DocumentsApi()
+    this.TemplatesApi = new TemplatesApi()
+    this.SignrequestsApi = new SignrequestsApi()
+    this.SignrequestQuickCreateApi = new SignrequestQuickCreateApi()
   }
-
-  async _call(section, method, data) {
+  
+  async _call (section, method, data) {
     return new Promise((resolve, reject) => {
       section[method](data, (error, data, response) => {
         if (error) {
           if (error && error.response && error.response.text) {
             error.message = error.response.text
           }
-
-          reject(error);
+          
+          reject(error)
         } else {
-          resolve(data);
+          resolve(data)
         }
       })
     })
   }
-
+  
   /**
    * @param {IUser} data
    * @return {ISigner[]}
    * @private
    */
-  _prepareSigners(data) {
+  _prepareSigners (data) {
     if (!data.mobile) {
-      throw new Error("Missing user mobile phone. Is required for user authentication when signing.")
+      throw new Error('Missing user mobile phone. Is required for user authentication when signing.')
     }
-
+    
     const toReturn = []
-    const userPhone = !data.mobile.toString().startsWith("+") ? "+39" + data.mobile : data.mobile
-
+    const userPhone = !data.mobile.toString().startsWith('+') ? '+39' + data.mobile : data.mobile
+    
     /** @type {Signer} */
     const firstSigner = {
       email: data.email,
       first_name: data.firstName,
       last_name: data.lastName,
-      verify_phone_number: Env.get("NODE_ENV") === "development" ? "" : userPhone,
-      language: data.language || "it",
+      verify_phone_number: Env.get('NODE_ENV') === 'development' ? '' : userPhone,
+      language: data.language || 'it'
       // after_document: "" // test what does it do.
     }
-
+    
     toReturn.push(Signer.constructFromObject(firstSigner))
-
+    
     return toReturn
   }
-
+  
   /**
    * @param {{}} data
    * @param {ITemplateConfig} template
    * @return {ITemplateConfig[]}
    * @private
    */
-  _prepareTagsData(data, template) {
+  _prepareTagsData (data, template) {
     const toReturn = []
-
+    
     for (const field of template.fields) {
       const valueTmpl = field.text
       let newValue
-
+      
       try {
         newValue = (_template(valueTmpl))(data)
       } catch (er) {
         // ignore any error that comes from the template compilation
-        newValue = ""
+        newValue = ''
       }
-
+      
       if (newValue) {
         toReturn.push({
           ...field,
@@ -106,22 +106,26 @@ class DocSigner {
         })
       }
     }
-
+    
     return toReturn
   }
-
-  async getTemplates() {
-    return this._call(this.TemplatesApi, "templatesList")
+  
+  async getTemplates () {
+    return this._call(this.TemplatesApi, 'templatesList')
   }
 
   async getDocuments() {
     return this._call(this.DocumentsApi, "documentsList")
   }
-
-  async deleteDocument(uuid) {
-    return this._call(this.DocumentsApi, "documentsDelete", uuid)
+  
+  async getDocument (uuid) {
+    return this._call(this.DocumentsApi, 'documentsRead', uuid)
   }
-
+  
+  async deleteDocument (uuid) {
+    return this._call(this.DocumentsApi, 'documentsDelete', uuid)
+  }
+  
   /**
    *
    * @param {ITemplateConfig} templateConfig
@@ -129,16 +133,16 @@ class DocSigner {
    * @param {string} existingRequest
    * @return {Promise<unknown>}
    */
-  async sendSignRequest(templateConfig, incomingData, existingRequest) {
-
+  async sendSignRequest (templateConfig, incomingData, existingRequest) {
+    
     // If already exists a sign request, first cancel it and the create the new one
     if (existingRequest) {
       try {
         /**
          * @type {{detail: "OK", cancelled: boolean}}
          */
-        const result = await this._call(this.SignrequestsApi, "signrequestsCancelSignrequest", existingRequest.uuid)
-
+        const result = await this._call(this.SignrequestsApi, 'signrequestsCancelSignrequest', existingRequest.uuid)
+        
         /*if (!result.cancelled) {
           throw new Error("Can't cancel the current sign request.")
         }*/
@@ -146,7 +150,7 @@ class DocSigner {
         throw er
       }
     }
-
+    
     /** @type {ISignRequestQuickCreate} */
     const signRequestData = {
       ...this._config.signRequestData,
@@ -159,17 +163,21 @@ class DocSigner {
       // message: "Messaggio del documento",
       // subject: "Richiesta di firma contratto per Mario Rossi",
     }
-
+    
     /**
      * @type {ISignRequestQuickCreate}
      */
-    const signRequestObj = SignRequestQuickCreate.constructFromObject(signRequestData);
-    const result = await this._call(this.SignrequestQuickCreateApi, "signrequestQuickCreateCreate", signRequestObj)
-
+    const signRequestObj = SignRequestQuickCreate.constructFromObject(signRequestData)
+    const result = await this._call(this.SignrequestQuickCreateApi, 'signrequestQuickCreateCreate', signRequestObj)
+    
     result.sentRequest = signRequestObj
-
+    
     return result
   }
 }
 
+/**
+ *
+ * @type {DocSigner}
+ */
 module.exports = DocSigner
